643c12782ed4aa6bfd49f07e31768914f96fcb84
Richard Seal <richyseal@gmail.com>
2021-11-10 14:32:24 -0500

    Fix button CTA text

--- pages/register.tsx
+++ pages/register.tsx
@@ -138,7 +138,7 @@ const Register: NextPageWithLayout = () => {
                       className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                       type="submit"
                     >
-                      Create Account
+                      Register Team
                     </button>
                   </div>
                 </form>
